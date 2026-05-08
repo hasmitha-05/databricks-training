6dd5aa66bddbdd806d76cf0fb3ff54b149ba80e7
hasmitha-05 <hasmithakothapalli5@gmail.com>
2026-05-08 17:26:58 +0530

    Add SQL queries for join operations
    Added multiple SQL queries for various join operations to analyze students, courses, and instructors.

--- week1/day2/joins.sql
+++ week1/day2/joins.sql
@@ -1,1 +1,74 @@
+--Question 1
+SELECT s.student_name,c.course_name
+FROM students s
+LEFT JOIN enrollments e
+ON s.student_id=e.student_id
+LEFT JOIN courses c
+ON e.course_id=c.course_id
+--Question 2
+SELECT c.course_name
+FROM courses c
+LEFT JOIN enrollments e
+ON c.course_id=e.course_id
+WHERE e.student_id IS NULL
+--Question 3
+SELECT i.instructor_name,c.course_name
+FROM instructors i
+LEFT JOIN courses c
+ON c.instructor_id=i.instructor_id
+--question 4
+SELECT *
+FROM courses c
+WHERE instructor_id IS NULL
+--Question 5
+SELECT *
+FROM students s
+RIGHT JOIN enrollments e
+ON s.student_id=e.student_id
+--Question 6
+SELECT s.student_name
+FROM students s
+LEFT JOIN enrollments e
+ON s.student_id=e.student_id
+WHERE e.course_id IS NULL
+--Question 7
+SELECT s.student_name,e.course_id
+FROM students s
+LEFT JOIN enrollments e
+ON s.student_id=e.student_id
 
+UNION
+
+SELECT s.student_name,e.course_id
+FROM students s
+RIGHT JOIN enrollments e
+ON s.student_id=e.student_id;
+--Question 8
+SELECT c.course_name
+FROM courses c
+LEFT JOIN enrollments e
+ON c.course_id=e.course_id
+WHERE e.course_id IS NULL;
+--Question 9
+SELECT c.course_name,i.instructor_name
+FROM courses c
+LEFT JOIN instructors i
+ON c.instructor_id=i.instructor_id
+UNION
+SELECT c.course_name,i.instructor_name
+FROM courses c
+RIGHT JOIN instructors i
+ON c.instructor_id=i.instructor_id
+--Question 10
+SELECT s.student_name,c.course_name,i.instructor_name
+FROM students s
+LEFT JOIN enrollments e
+ON s.student_id=e.student_id
+LEFT JOIN courses c
+ON c.course_id=e.course_id
+LEFT JOIN instructors i
+ON c.instructor_id=i.instructor_id
+--Bonus Challenge
+SELECT s.student_name,c.course_name
+FROM students s
+CROSS JOIN courses c;
