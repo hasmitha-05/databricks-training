025d74dc19212cf7ae97807a9366eae874b1897b
hasmitha-05 <hasmithakothapalli5@gmail.com>
2026-05-24 10:21:14 +0530

    Add queries for handling null values in SQL

--- week2/day-null_functions/query.sql
+++ week2/day-null_functions/query.sql
@@ -1,1 +1,10 @@
+select * from Employees where salary is null
+select * from Orders where discount is not null
+select * from Products where category is null
+select count(emp_id) from Employees where manager_id is null
+select ifnull(salary,0) from Employees;
+select ifnull(bonus,1000) from Employees;
+select amount, ifnull(amount,500) from Orders;
+select ifnull(stock,0) from Products;
+select name, coalesce(salary,bonus) as earnings from Employees
 
