ad9757b787a9abe8fcaaca4e39a30ca3640e7234
hasmitha-05 <hasmithakothapalli5@gmail.com>
2026-07-12 17:24:06 +0530

    Implement revenue calculation by city in PySpark

--- phase-2/PySpark/exercise-4.py
+++ phase-2/PySpark/exercise-4.py
@@ -1,1 +1,14 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, sum
 
+spark = SparkSession.builder.appName("Exercise4_PySpark").getOrCreate()
+customers = spark.read.option("header", "true").csv("/samples/customers.csv")
+sales = spark.read.option("header", "true").csv("/samples/sales.csv")
+
+sales = sales.withColumn("total_amount", col("total_amount").cast("double"))
+result = customers.join(sales,"customer_id")\
+          .groupBy("city")\
+          .agg(sum("total_amount").alias("totalRevenue"))\
+          .orderBy(col("total_revenue").desc())
+
+result.show()
