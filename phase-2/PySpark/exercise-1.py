78459416cc9fc59779ddceed084caaadac25e2e2
hasmitha-05 <hasmithakothapalli5@gmail.com>
2026-07-12 16:47:47 +0530

    Add PySpark code to calculate total sales by customer

--- phase-2/PySpark/exercise-1.py
+++ phase-2/PySpark/exercise-1.py
@@ -1,1 +1,9 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import col, sum
 
+spark = SparkSession.builder.appName("Exercise1_PySpark").getOrCreate()
+sales = spark.read.option("header", "true").csv("/samples/sales.csv")
+
+sales = sales.withColumn("total_amount",col("total_amount").cast("double"))
+result = sales.groupBy("customer_id").agg(sum("total_amount").alias("totalSales"))
+result.show()
