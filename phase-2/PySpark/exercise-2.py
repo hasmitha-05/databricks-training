8232a468a92964b8ca13eb62892b619ebd43c5b6
hasmitha-05 <hasmithakothapalli5@gmail.com>
2026-07-12 16:58:13 +0530

    Add PySpark code to analyze sales data

--- phase-2/PySpark/exercise-2.py
+++ phase-2/PySpark/exercise-2.py
@@ -1,1 +1,6 @@
-
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import sum
+spark = SparkSession.builder.appName("Sales").getOrCreate()
+sales = spark.read.csv("/samples/sales.csv", header=True, inferSchema=True)
+result = sales.groupBy("customer_id").sum("total_amount")
+result.orderBy("sum(total_amount)", ascending=False).show(3)
