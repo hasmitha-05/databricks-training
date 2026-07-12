fc0c1aad32a39af70f13b6fed0812a4fab09b86a
hasmitha-05 <hasmithakothapalli5@gmail.com>
2026-07-12 17:29:35 +0530

    Implement sales aggregation and filtering in PySpark

--- phase-2/PySpark/exercise-6.py
+++ phase-2/PySpark/exercise-6.py
@@ -1,1 +1,12 @@
+from pyspark.sql import SparkSession
+from pyspark.sql.functions import count, col
 
+spark = SparkSession.builder.appName("Exercise6_PySpark").getOrCreate()
+sales = spark.read.option("header", "true").csv("/samples/sales.csv")
+
+result = sales.groupBy("customer_id") \
+    .agg(count("sale_id").alias("totalSales")) \
+    .filter(col("total_sales") > 1) \
+    .orderBy(col("total_sales").desc())
+
+result.show()
